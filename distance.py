c2cf064e75742e85f9aa5211cea68fa681ed70bb
Luke <lukedee@gmail.com>
2025-07-10 17:16:15 +0000

    initial commit

---
+++ distance.py
@@ -0,0 +1,5 @@
+# https://pypi.org/project/sgp4/
+# You are to gather the TLE data for two satellites and a specific timestamp. Your task is to compute the distance (in kilometers) between them at that time.
+
+def compare_satellite_distance(tle1: Tuple[str, str], tle2: Tuple[str, str], timestamp: str) -> float:
+    pass
